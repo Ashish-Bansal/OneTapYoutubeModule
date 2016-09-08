72e161a9bbe15cb2b5e1217aae7984b73cb055b1
Ashish Bansal <bansal.ashish096@gmail.com>
2016-09-09 01:21:49 +0530

    Removed Youtube Media Hooking
    This project has been merged into One Tap Video Download. Just for the
    sake of the consistency as well as to continue the legacy, this *empty*
    module will still be kept and users will have to install it to enable
    youtube video downloading in the main project(One Tap Video Download).

---
+++ app/src/main/java/com/phantom/onetapyoutubemodule/YoutubeEnableHook.java
@@ -0,0 +1,9 @@
+package com.phantom.onetapyoutubemodule;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class YoutubeEnableHook implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    }
+}
